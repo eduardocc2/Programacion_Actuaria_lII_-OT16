f9dc5179557f61f4ffd8bb13c9c418487cf46940
eduardocc2 <contreras7febrero@gmail.com>
2016-09-05 09:29:43 -0500

    Clase Airq
    Subir un documento especifico de r

---
+++ airq.R
@@ -0,0 +1,5 @@
+structure(list(Ozone = c(41L, 36L, 12L, 18L, NA, 28L), Solar.R = c(190L, 
+118L, 149L, 313L, NA, NA), Wind = c(7.4, 8, 12.6, 11.5, 14.3, 
+14.9), Temp = c(67L, 72L, 74L, 62L, 56L, 66L), Month = c(5L, 
+5L, 5L, 5L, 5L, 5L), Day = 1:6), .Names = c("Ozone", "Solar.R", 
+"Wind", "Temp", "Month", "Day"), row.names = c(NA, 6L), class = "data.frame")
